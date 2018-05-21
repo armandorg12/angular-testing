4c834b3a5e12e3c381ca73ad88d952203b78d70a
jorgeluishg <u14103018@alumnos.univo.edu.mx>
2018-05-21 13:00:08 -0500

    compute feature

---
+++ src/app/compute/compute.spec.ts
@@ -0,0 +1,14 @@
+import { compute } form './compute' ;
+
+describe (' compute ' , () => { // suite
+
+it ('should return 0 if input is negative', () => {
+		const result = compute ( -1);
+		expect(result).toBe(0);
+})
+
+it('should increment if input is positive ', () => {
+		const result = compute(1);
+		expect(result).toBe(2):
+})
+})
